2cfb24f3659fd6aa2685ef8367d732e65bda2c38
Alchrist <alchrist@student.21-school.ru>
2022-05-22 11:59:26 +0300

    add maket with all buttons!!!

--- game/views.py
+++ game/views.py
@@ -1,4 +1,20 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(None, 'game/index.html')
+
+def move(request, direction):
+    return HttpResponse("move to the " + direction)
+
+def start(request):
+    return HttpResponse("pressed start")
+
+def select(request):
+    return HttpResponse("pressed select")
+
+def btn_a(request):
+    return HttpResponse("pressed A")
+
+def btn_b(request):
+    return HttpResponse("pressed B")
