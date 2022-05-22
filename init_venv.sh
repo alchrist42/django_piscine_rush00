bb8ba0e95452a13130b253f7466e48f85f4182fc
Alchrist <alchrist@student.21-school.ru>
2022-05-22 10:08:47 +0300

    move project to root folder

--- init_venv.sh
+++ init_venv.sh
@@ -6,4 +6,4 @@ VENV_DIR="django_venv"
 python3.10 -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
 
-pip  install --force-reinstall -r requirement.txt
+pip  install --force-reinstall -r requirements.txt
---
+++ run_venv.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+VENV_DIR="django_venv"
+source $VENV_DIR/bin/activate
